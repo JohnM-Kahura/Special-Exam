69a2ed9364bd31f6ae224ac62f2e100faff502af
JohnM-Kahura <Johnmungai392@gmail.com>
2022-01-08 14:24:33 +0300

    links to pages to be scraped

---
+++ BackEnd/Python/fetch.py
@@ -0,0 +1,27 @@
+import requests
+from requests.sessions import _Data
+from bs4 import BeautifulSoup
+
+#  https://registration.ueab.ac.ke/ueab/j_security_check
+# https://registration.ueab.ac.ke/ueab/a_students.jsp?view=1:0
+login_url=('https://registration.ueab.ac.ke/ueab/j_security_check')
+dashboard=('https://registration.ueab.ac.ke/ueab/a_students.jsp?view=1:0')
+profile=('https://registration.ueab.ac.ke/ueab/a_students.jsp?view=2:0')
+school_info=('https://registration.ueab.ac.ke/ueab/a_students.jsp?view=10:0')
+
+username='SKAHMU2011'
+password='886I882OW'
+payload={
+    'j_username':username,
+    'j_password':password,
+}
+with requests.sessions() as s:
+    s.post(login_url,data=payload)
+    dashboard_request=s.get(dashboard)
+    profile_request=BeautifulSoup(profile,'lxml')
+    school_info_request=BeautifulSoup(school_info,'lxml')
+    dash_soup=BeautifulSoup(dashboard_request.content,'lxml')
+    profile_soup=BeautifulSoup(profile_request.content,'lxml')
+    school_info_soup=BeautifulSoup(school_info_request.content,'lxml')
+
+    
